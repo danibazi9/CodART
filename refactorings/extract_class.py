e3a0d9f602e6931d24b5233532e4392d0a9d814b
Danial <danibazi9@gmail.com>
2021-01-30 04:20:18 +0330

    Some changes to improve the code readability

--- refactorings/extract_class.py
+++ refactorings/extract_class.py
@@ -24,26 +24,27 @@ class ExtractClassRecognizerListener(Java9_v2Listener):
     To detect whether class needs refactoring or not.
     """
 
-    def __init__(self, common_token_stream: CommonTokenStream = None,
-                 class_identifier: str = None):
+    def __init__(self, common_token_stream: CommonTokenStream = None, class_identifier: str = None):
         """
         :param common_token_stream:
         """
         self.enter_class = False
         self.token_stream = common_token_stream
         self.class_identifier = class_identifier
+
         # Move all the tokens in the source code in a buffer, token_stream_rewriter.
         if common_token_stream is not None:
             self.token_stream_rewriter = TokenStreamRewriter(common_token_stream)
         else:
             raise TypeError('common_token_stream is None')
+
         self.field_dict = {}
-        self.method_name = []  #
+        self.method_name = []
         self.method_no = 0
 
-    # Groups methods in terms of their dependncies on the class attributes and one another
+    # Groups methods in terms of their dependencies on the class attributes and one another
     def split_class(self):
-        # 1- move the dictionay of fields into a new dictionary of methods operating on fields
+        # 1- move the dictionary of fields into a new dictionary of methods operating on fields
         method_dict = {}
         for key, value in self.field_dict.items():
             for method in value:
@@ -51,9 +52,12 @@ class ExtractClassRecognizerListener(Java9_v2Listener):
                     method_dict[str(method)] = [key]
                 else:
                     method_dict[str(method)].append(key)
+
         print("methods dic.", method_dict)
+
         # 2- Group methods in terms of their dependencies on one another
         method_group = dict()
+
         # _____________________To be modified ________________________
         # 3- Group methods in terms of their dependencies on the class attributes
         for key, value in method_dict.items():
@@ -134,11 +138,13 @@ class ExtractClassRecognizerListener(Java9_v2Listener):
             return
         if self.method_no == 0:
             return
+
         current_method = self.method_name[-1]
         variable_name = ctx.getText()
+
         if variable_name not in self.field_dict:
             return
-        if not current_method in self.field_dict[variable_name]:
+        if current_method not in self.field_dict[variable_name]:
             self.field_dict[variable_name].append(current_method)
 
 
@@ -148,14 +154,14 @@ class ExtractClassRefactoringListener(Java9_v2Listener):
     Creates a new class and move fields and methods from the old class to the new one
     """
 
-    def __init__(
-            self, common_token_stream: CommonTokenStream = None,
-            source_class: str = None, new_class: str = None,
-            moved_fields=None, moved_methods=None):
+    def __init__(self, common_token_stream: CommonTokenStream = None,
+                 source_class: str = None, new_class: str = None,
+                 moved_fields=None, moved_methods=None):
         if moved_methods is None:
             self.moved_methods = []
         else:
             self.moved_methods = moved_methods
+
         if moved_fields is None:
             self.moved_fields = []
         else:
@@ -170,6 +176,7 @@ class ExtractClassRefactoringListener(Java9_v2Listener):
             raise ValueError("source_class is None")
         else:
             self.source_class = source_class
+
         if new_class is None:
             raise ValueError("new_class is None")
         else:
@@ -185,6 +192,7 @@ class ExtractClassRefactoringListener(Java9_v2Listener):
     def enterNormalClassDeclaration(self, ctx: Java9_v2Parser.NormalClassDeclarationContext):
         print("Refactoring started, please wait...")
         class_identifier = ctx.identifier().getText()
+
         if class_identifier == self.source_class:
             self.is_source_class = True
             self.code += self.NEW_LINE * 2
@@ -215,25 +223,25 @@ class ExtractClassRefactoringListener(Java9_v2Listener):
     def exitFieldDeclaration(self, ctx: Java9_v2Parser.FieldDeclarationContext):
         if not self.is_source_class:
             return None
+
         field_names = ctx.variableDeclaratorList().getText().split(",")
         if self.detected_field in field_names:
             modifier = ctx.fieldModifier(0).getText()
             field_type = ctx.unannType().getText()
             self.code += f"{self.TAB}{modifier} {field_type} {self.detected_field};{self.NEW_LINE}"
+
             # delete field from source class
             field_names.remove(self.detected_field)
             if field_names:
-                self.token_stream_rewriter.replaceRange(
-                    from_idx=ctx.start.tokenIndex,
-                    to_idx=ctx.stop.tokenIndex,
-                    text=f"{modifier} {field_type} {','.join(field_names)};"
-                )
+                self.token_stream_rewriter.replaceRange(from_idx=ctx.start.tokenIndex,
+                                                        to_idx=ctx.stop.tokenIndex,
+                                                        text=f"{modifier} {field_type} {','.join(field_names)};"
+                                                        )
             else:
-                self.token_stream_rewriter.delete(
-                    program_name=self.token_stream_rewriter.DEFAULT_PROGRAM_NAME,
-                    from_idx=ctx.start.tokenIndex,
-                    to_idx=ctx.stop.tokenIndex
-                )
+                self.token_stream_rewriter.delete(program_name=self.token_stream_rewriter.DEFAULT_PROGRAM_NAME,
+                                                  from_idx=ctx.start.tokenIndex,
+                                                  to_idx=ctx.stop.tokenIndex
+                                                  )
             self.detected_field = None
 
     def enterMethodDeclarator(self, ctx: Java9_v2Parser.MethodDeclaratorContext):
@@ -250,16 +258,18 @@ class ExtractClassRefactoringListener(Java9_v2Listener):
         if self.detected_method == method_identifier:
             start_index = ctx.start.tokenIndex
             stop_index = ctx.stop.tokenIndex
+
             method_text = self.token_stream_rewriter.getText(
                 program_name=self.token_stream_rewriter.DEFAULT_PROGRAM_NAME,
                 start=start_index,
                 stop=stop_index
             )
+
             self.code += (self.NEW_LINE + self.TAB + method_text + self.NEW_LINE)
+
             # delete method from source class
-            self.token_stream_rewriter.delete(
-                program_name=self.token_stream_rewriter.DEFAULT_PROGRAM_NAME,
-                from_idx=start_index,
-                to_idx=stop_index
-            )
+            self.token_stream_rewriter.delete(program_name=self.token_stream_rewriter.DEFAULT_PROGRAM_NAME,\
+                                              from_idx=start_index,
+                                              to_idx=stop_index
+                                              )
             self.detected_method = None
