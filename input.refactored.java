54493c32cc1983f62e3e517e3ab7bdcb95b305b8
unknown <github.edu@hotmail.com>
2021-02-02 20:09:46 +0330

    screenshot REWT

--- input.refactored.java
+++ input.refactored.java
@@ -1,21 +1,15 @@
-public class Sth {
+public class Main {
     public static void main(String[] args) {
         try {
             int[] numbers = new int[]{1, 2, 3};
             int index = 4;
             int i; System
-                    .out
-                    .println(numbers[index]); int j; //IndexOutOfBoundsException
+                .out
+                .println(numbers[index]); int j; //IndexOutOfBoundsException
         }
         catch (IndexOutOfBoundsException exception){
             System.out.println("IndexOutOfBoundsException Caught...");
             String index = "0";
-        }
-        catch (ArrayStoreException exception){
-            //sth ...
-        }
-        finally{
-            //sth ...
         }
     }
 }
--- refactorings/test/test3.java
+++ refactorings/test/test3.java
@@ -1,24 +1,17 @@
-public class Sth {
+public class Main {
     public static void main(String[] args) {
-        try {
-            int[] numbers = new int[]{1, 2, 3};
-            int index = 4;
-            int i;
-if (index < numbers.length) {
-	 System
-	                    .out
+
+        int[] numbers = new int[]{1, 2, 3};
+        int index = 4;
+        int i;
+        if (index < numbers.length) {
+            System
+                    .out
                     .println(numbers[index]); }
- else {
-            System.out.println("IndexOutOfBoundsException Caught...");
-            String index = "0";
+        else {
+            System.out.println("IndexOutOfBoundsException Caught...");
+            String index = "0";
         }
- int j; //IndexOutOfBoundsException
-        }
-        catch (ArrayStoreException exception){
-            //sth ...
-        }
-        finally{
-            //sth ...
-        }
+        int j; //IndexOutOfBoundsException
     }
 }
