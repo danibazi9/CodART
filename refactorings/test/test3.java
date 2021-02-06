c0d69e65c715ac090602e6705564ce36d18bbbac
unknown <github.edu@hotmail.com>
2021-02-06 15:00:26 +0330

    Quick Java Grammar Applied...

--- refactorings/test/test3.java
+++ refactorings/test/test3.java
@@ -1,17 +1,21 @@
-public class Main {
+public class Sth {
     public static void main(String[] args) {
-
-        int[] numbers = new int[]{1, 2, 3};
-        int index = 4;
-        int i;
-        if (index < numbers.length) {
-            System
+        try {
+            int[] numbers = new int[]{1, 2, 3};
+            int index = 4;
+            int i; System
                     .out
-                    .println(numbers[index]); }
-        else {
+                    .println(numbers[index]); int j; //IndexOutOfBoundsException
+        }
+        catch (IndexOutOfBoundsException exception){
             System.out.println("IndexOutOfBoundsException Caught...");
             String index = "0";
         }
-        int j; //IndexOutOfBoundsException
+        catch (ArrayStoreException exception){
+            //sth ...
+        }
+        finally{
+            //sth ...
+        }
     }
 }
